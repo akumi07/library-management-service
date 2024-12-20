b20abcb9d4355a717b2fcf6cdbfbce64f49616c1
akumi07 <akash9338144396@gmail.com>
2024-12-20 22:45:07 +0530

    query 1

--- quer1.sql
+++ quer1.sql
@@ -3,3 +3,61 @@ select * FROM branch;
 select * FROM employees;
 select * FROM issued_status;
 select * FROM members;
+
+-- ### 2. CRUD Operations
+
+
+-- Task 1. Create a New Book Record
+-- "978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.')"
+
+-- Task 2: Update an Existing Member's Address
+
+
+-- Task 3: Delete a Record from the Issued Status Table
+-- Objective: Delete the record with issued_id = 'IS104' from the issued_status table.
+
+-- Task 4: Retrieve All Books Issued by a Specific Employee
+-- Objective: Select all books issued by the employee with emp_id = 'E101'.
+
+
+-- Task 5: List Members Who Have Issued More Than One Book
+-- Objective: Use GROUP BY to find members who have issued more than one book.
+
+
+-- ### 3. CTAS (Create Table As Select)
+
+-- Task 6: Create Summary Tables**: Used CTAS to generate new tables based on query results - each book and total book_issued_cnt
+
+
+-- ### 4. Data Analysis & Findings
+
+-- Task 7. **Retrieve All Books in a Specific Category:
+
+
+-- Task 8: Find Total Rental Income by Category:
+
+
+-- Task 9. **List Members Who Registered in the Last 180 Days**:
+
+-- Task 10: List Employees with Their Branch Manager's Name and their branch details**:
+
+
+-- Task 11. Create a Table of Books with Rental Price Above a Certain Threshold
+
+-- Task 12: Retrieve the List of Books Not Yet Returned
+
+    
+/*
+
+
+INSERT INTO books(isbn,
+    book_title ,
+	category ,
+	rental_price ,
+	status ,
+	author ,
+	publisher)
+    VALUES( '978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.')
+
+    select * from books
+    
